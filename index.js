068aa2a97f8f0ce4c19a4afe141b41f54c775c4b
ogneyar <ogneyar-ne@ya.ru>
2021-04-11 17:35:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const WSServer = require('express-ws')(app)
+const aWss = WSServer.getWss()
+
+const PORT = process.env.PORT || 5000
+
+app.ws('/', (ws, req) => {
+    ws.on('message', (msg) => {
+        msg = JSON.parse(msg)
+        switch (msg.method) {
+            case 'connection':
+                connectionHandler(ws, msg)
+                break
+            case 'draw':
+                broadcastConnection(ws, msg)
+                break
+        }
+    })
+})
+
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+
+const broadcastConnection = (ws, msg) => {
+    aWss.clients.forEach(client => {
+        if (client.id === msg.id) {
+            client.send(JSON.stringify(msg))
+        }
+    })
+}
+
+const connectionHandler = (ws, msg) => {
+    ws.id = msg.id
+    broadcastConnection(ws, msg)
+}
